7c2f9bedff9343e604c1c8f30eb7ebc19841ea10
Geoffrey RIGAL <g.rigal@comutitres.fr>
2025-06-12 15:01:44 +0200

    init symfony doctrine fixture entity controller

---
+++ src/Controller/BookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BookController extends AbstractController
+{
+    #[Route('/api/books', name: 'book')]
+    public function getAllBooks(): JsonResponse
+    {
+        return new JsonResponse([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/BookController.php',
+        ]);
+    }
+}
